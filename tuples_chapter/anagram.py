193c2c07b81c5921d476fc8d4590e3c139e6695a
SREEHARI1994 <sreehari.mras@gmail.com>
2020-01-27 15:11:40 +0530

    anagram excercise completed

--- tuples_chapter/anagram.py
+++ tuples_chapter/anagram.py
@@ -1,6 +1,5 @@
 """
 this is the solution to the excercise on finding anagrams from word list, 
-for solution to second part refer anagram_sets.py
 """
 def make_word_list():
     words=[]
@@ -22,22 +21,40 @@ def find_anagrams(words):
             d[k].append(word)
     return d
 
+def print_anagrams(d):
+	for value in d.values():
+		if len(value) > 1:
+			print(len(value),value)
+
+
+def print_anagram_inorder(d):
+	res_list=[]
+	for value in d.values():
+		if len(value) > 1:
+			res_list.append((len(value),value))
+	res_list.sort()
+	for res in res_list:
+		print(res)
+
+def filtered_dict(d,n):
+	new_dict={}
+	for key,value in d.items():
+		if len(key)==n:
+			new_dict[key]=value
+	return new_dict
 
 if __name__ == "__main__":
-    word_list=make_word_list()
-   
-    res_dict=find_anagrams(word_list)
-    #modified commented out code for printing in order of length
-    #for value in res_dict.values():
-    res_list=[]
-    res_list.sort()
-    for value in res_dict.values():
-        if len(value) > 1:
-            res_list.append((len(value),value))
-    res_list.sort()
-    for res in res_list:
-        print(res)
-            
+	word_list=make_word_list()
+	res_dict=find_anagrams(word_list)
+	print_anagrams(res_dict)
+	print("Now printing anagrams in order of increasing length\n")
+	print_anagram_inorder(res_dict)
+	print("solution to bingo problem\n")
+	bingo_dict=filtered_dict(res_dict,8)
+	
+	print_anagram_inorder(bingo_dict)
+	print("The end")
+
 
 
 
